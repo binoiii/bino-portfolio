4e99a12c748b0c0133ab80515b3ec4fb5e110e2e
binothird <albinoiii.cordova@gmail.com>
2021-01-03 22:42:11 +0800

    added thankyou page

--- src/components/Utilities/BackButton.js
+++ src/components/Utilities/BackButton.js
@@ -3,14 +3,14 @@ import PropTypes from "prop-types"
 import { Link } from "gatsby"
 import { BackIcon } from "../Icons/Icons"
 
-const BackButton = ({ location }) => (
+const BackButton = ({ label, location }) => (
   <div className="mb-4 flex justify-end">
     <Link
       to={`/${location}`}
       className="text-yellow-500 transition duration-300 ease-out transform hover:-translate-x-2 hover:text-blue-450"
     >
       <BackIcon className="inline text-lg md:text-xl " />
-      <span className="m-2 font-mono tracking-wider">Back</span>
+      <span className="m-2 font-mono tracking-wider">{label}</span>
     </Link>
   </div>
 )
@@ -19,8 +19,10 @@ export default BackButton
 
 BackButton.defaultProps = {
   location: "",
+  label: "Back",
 }
 
 BackButton.propTypes = {
   location: PropTypes.string,
+  label: PropTypes.string,
 }
