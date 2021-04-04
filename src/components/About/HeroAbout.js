5afce264b3d0b8b2682798b51886fd9048b9dab4
binothird <albinoiii.cordova@gmail.com>
2021-04-05 01:17:46 +0800

    updated resume link

--- src/components/About/HeroAbout.js
+++ src/components/About/HeroAbout.js
@@ -20,7 +20,7 @@ const HeroAbout = () => {
       <div className="flex">
         <div className="py-2 px-2 md:px-6 mr-2 bg-blue-450 border-2 border-blue-450 font-primary text-xs md:text-base text-white tracking-wide cursor-pointer hover:bg-gray-100 hover:text-blue-450 ">
           <a
-            href="https://drive.google.com/file/d/1cbD58ws3lARaO73sVcvoQUSO-0PUFkGv/view?usp=sharing"
+            href="https://drive.google.com/file/d/1GImMwfPcZfXQzGj86ReKarfZ0SPMfnIU/view?usp=sharing"
             download
           >
             See resume
